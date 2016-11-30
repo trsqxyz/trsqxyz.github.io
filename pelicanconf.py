9ea29c16bf86bd2f05b3b93bcbb292c413a5d82a
trsqxyz <trsqxyz@gmail.com>
2016-11-30 14:42:40 +0900

    Add tags to menu items.

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,5 +42,9 @@ THEME = 'mytheme/notmyidea'
 TWITTER_USERNAME = 'trss'
 DISPLAY_CATEGORIES_ON_MENU = False
 
+MENUITEMS =(('Archives', '/archives.html'),
+            ('Tags', 'tags.html'),
+            )
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
